2dfeaadc9921a11a6fd434cebec9a8df5c66b299
Akash-Verma96 <av3764909@gmail.com>
2026-01-15 22:58:46 +0530

    Database Changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const connectDB = async function(){
-    await mongoose.connect('mongodb://localhost:27017/devTinder');
+    // await mongoose.connect('mongodb://localhost:27017/devTinder');
+    await mongoose.connect('mongodb+srv://Akash-Webdev:Akash96%4096@cluster0.u9gqqds.mongodb.net/devTinder');
 }
 
 module.exports = {
